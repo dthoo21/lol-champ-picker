7bcc01a0848cb1af8c0761acbc174ad9825d8466
dthoo21 <daniel.thoo21@gmail.com>
2020-08-10 12:13:44 -0400

    commented and clean up

--- champ_picker.py
+++ champ_picker.py
@@ -3,37 +3,30 @@ import sys
 import time
 
 select = False
-screen_width, screen_height = pyautogui.size()
+screen_width, screen_height = pyautogui.size() # Measure screen size for multiple screen resolution support
 
-# pyautogui.click('search_text.png')
+print("#############################\n# Daniel's LoL Champ Picker #\n#############################\n")
 
-# print("Found search bar")
-
-# pyautogui.write(sys.argv[1])
-# print("Entered champion name")
-
-# if screen_width == 1920 and screen_height == 1080:
-#     pyautogui.move(-390, 64, duration=0.2)
-#     pyautogui.click(duration=0.2)
-
-# pyautogui.click('lock_in.png')
-
-while (select == False):
+while (select == False): # Repeat while operation has failed
     try:
-        pyautogui.click('search_text.png')
+        pyautogui.click('search_text.png') # Find and select the search bar
         print("Found search bar")
 
-        pyautogui.write(sys.argv[1])
+        pyautogui.write(sys.argv[1]) # Write the argument
         print("Entered champion name")
 
-        if screen_width == 1920 and screen_height == 1080:
-            pyautogui.move(-390, 64, duration=0.2)
-            pyautogui.click(duration=0.2)
+        if screen_width == 1920 and screen_height == 1080: # For 1080p screen
+            pyautogui.move(-390, 64, duration=0.2) # Move from search bar to champion icon
+            pyautogui.click(duration=0.2) # Select champion icon
+            print("Selected champion icon")
 
-        pyautogui.click('lock_in.png')
+        # 720p support to be added
+
+        pyautogui.click('lock_in.png') # Find and select lock in button
+        print("Locked in")
         
-        select = True
+        select = True # Cease loop. Operation has succeeded
 
     except:
         print("Cannot not find elements. Sleep for 1 seconds.")
-        time.sleep(1)
+        time.sleep(1) # Limit loop speed
