08b87f60ee9e0c3338d401b57c35b5525d3189ad
dthoo21 <daniel.thoo21@gmail.com>
2020-08-10 13:57:19 -0400

    catch specific error instead of all exceptions

--- champ_picker.py
+++ champ_picker.py
@@ -17,15 +17,17 @@ while (select == False): # Repeat while operation has failed
 
         if screen_width == 1920 and screen_height == 1080: # For 1080p screen
             pyautogui.move(-390, 64, duration=0.2) # Move from search bar to champion icon
-            pyautogui.click(duration=0.2) # Select champion icon
+            pyautogui.click() # Select champion icon
             print("Selected champion icon")
 
         # 720p support to be added
 
-        pyautogui.click('lock_in.png') # Find and select lock in button
+        pyautogui.move(257, 439, duration=0.2)
+        pyautogui.click()
+        # pyautogui.click('lock_in.png') # Find and select lock in button
         print("Locked in")
         
         select = True # Cease loop. Operation has succeeded
 
-    except:
+    except TypeError as e:
         print("Cannot not find elements. Looping.")
