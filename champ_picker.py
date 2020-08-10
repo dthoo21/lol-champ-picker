8f76082a0ef4873167698f96ce0267e96ab7a21f
dthoo21 <daniel.thoo21@gmail.com>
2020-08-10 12:04:42 -0400

    exception catching and background operation

--- champ_picker.py
+++ champ_picker.py
@@ -1,19 +1,39 @@
 import pyautogui
 import sys
+import time
 
+select = False
 screen_width, screen_height = pyautogui.size()
 
+# pyautogui.click('search_text.png')
 
+# print("Found search bar")
 
-pyautogui.click('search_text.png')
+# pyautogui.write(sys.argv[1])
+# print("Entered champion name")
 
-print("Found search bar")
+# if screen_width == 1920 and screen_height == 1080:
+#     pyautogui.move(-390, 64, duration=0.2)
+#     pyautogui.click(duration=0.2)
 
-pyautogui.write(sys.argv[1])
-print("Entered champion name")
+# pyautogui.click('lock_in.png')
 
-if screen_width == 1920 and screen_height == 1080:
-    pyautogui.move(-390, 64, duration=0.2)
-    pyautogui.click(duration=0.2)
+while (select == False):
+    try:
+        pyautogui.click('search_text.png')
+        print("Found search bar")
 
-pyautogui.click('lock_in.png')
+        pyautogui.write(sys.argv[1])
+        print("Entered champion name")
+
+        if screen_width == 1920 and screen_height == 1080:
+            pyautogui.move(-390, 64, duration=0.2)
+            pyautogui.click(duration=0.2)
+
+        pyautogui.click('lock_in.png')
+        
+        select = True
+
+    except:
+        print("Cannot not find elements. Sleep for 1 seconds.")
+        time.sleep(1)
